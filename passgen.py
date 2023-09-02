bdeb47cf368f92345ed68970a7bb5313f68fb75b
PlantBasedCodes <128643124+PlantBasedCodes@users.noreply.github.com>
2023-09-01 21:34:06 -0400

    added slow type to passgen.py
    slow text

--- passgen.py
+++ passgen.py
@@ -2,6 +2,7 @@
 import time
 from time import sleep
 import random
+import sys
 
 #code
 
@@ -21,8 +22,20 @@ backupcode = ''
 for x in range(6):
     backupcode += random.choice(backup)
 
-print('Username Password & Security Code Generator')
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+print_slow("Username Password & Security Code Generator \n")
 sleep(.75)
-print('Very basic ')
-sleep(2)
+print_slow("Very basic \n")
+sleep(.75)
+print_slow("Source @ https://github.com/PlantBasedCodes/python/blob/main/passgen.py \n")
+sleep(3)
 print('\n Your password: ' + password +  '\n Your username: ' + username + '\n You backup codes: ' + backupcode + '\n')
+sleep(.95)
+print('thank you for using this \n')
+sleep(.75)
+print('- Chris \n')
