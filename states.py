9558a7d96a94a3eeb56291877c1f2163af4c24c9
PlantBasedCodes <128643124+PlantBasedCodes@users.noreply.github.com>
2023-09-02 13:27:31 -0400

    Update states.py

--- states.py
+++ states.py
@@ -7,106 +7,106 @@ import random
 
 #code start ig!!
 
-state = input('Enter ur state CASE SENS: ')
+while True:
+    state = input('Enter ur state CASE SENS: ')
 
+    if state == 'North Carolina':
+        print('look outside bro')
+    elif state == 'Alabama':
+        print('ur screwing ur sister bro')
+    elif state == 'Alaska':
+        print('snowing')
+    elif state == 'Arizona':
+        print('ur making eggs in ur car w out a oven!!')
+    elif state == 'California':
+        print('ur a freaking softie?!?!????!!????? idk i kinda forgor')
+    elif state == 'Colorado':
+        print('ur skiing rn bro')
+    elif state == 'Connecticut':
+        print('ur either rich or in the hood :skull:')
+    elif state == 'Delaware':
+        print('unknown fr fr')
+    elif state == 'Florida':
+        print('u swimming with sharks?? idk bro')
+    elif state == 'Georgia':
+        print('ur growing mangos bruh STOP')
+    elif state == 'Hawaii':
+        print('stop surfing')
+    elif state == 'Idaho':
+        print('ur the hoe bruh')
+    elif state == 'Illinois':
+        print('unknown fr :skull:')
+    elif state == 'Indiana':
+        print('ur that one guy from that movie broooo')
+    elif state == 'Iowa':
+        print('another unknown :frown:')
+    elif state == 'Kansas':
+        print('kanasa city?!?!')
+    elif state == 'Kansas':
+        print('Kansas city?!?!?')
+    elif state == 'Kentucky':
+        print('Kentucky ballistics??')
+    elif state == 'Louisiana':
+        print('river')
+    elif state == 'Maine':
+        print('r u like fishing')
+    elif state == 'Maryland':
+        print('marry me get it haha')
+    elif state == 'Massachutetts':
+        print('ur the best')
+    elif state == 'Michigan':
+        print('MIchiaginas')
+    elif state == 'Mississippi':
+        print('another river')
+    elif state == 'Missori':
+        print('unknown')
+    elif state == 'Montana':
+        print('hmm another unknown')
+    elif state == 'Nebraska':
+        print('hmm another unknown!!')
+    elif state == 'Nevada':
+        print('gamble')
+    elif state == 'New hampshire':
+        print('i love u bro pls flip me till i leak all over the place :love:')
+    elif state == 'New Jersy':
+        print('idk i forgor what u have')
+    elif state == 'New Mexico':
+        print('Breaking bad')
+    elif state == 'North Dakota':
+        print('hmm another unknown')
+    elif state == 'Ohio':
+        print('Sigma skibidi toilet omg lvl 5 gyatt kai centait etc!!')
+    elif state == 'Oklahoma':
+        print('Only in oklahoma')
+    elif state == 'Oregon':
+        print('nice lakes and mountineasasasa')
+    elif state == 'Pennsylvania':
+        print('Good mexican food for some reason')
+    elif state == 'Rhode island':
+        print('that one beach ball game')
+    elif state == 'South Carolina':
+        print('ass beach :skull: north myrtle better lmao')
+    elif state == 'South Dakota':
+        print('hmm another unknown')
+    elif state == 'Tennessee':
+        print('fgtv')
+    elif state == 'Texas':
+        print('guns i love u')
+    elif state == 'Utah':
+        print('hmm another unknown')
+    elif state == 'Vermont':
+        print('haha ur nice')
+    elif state == 'Washington':
+        print('idk candadaadadadada')
+    elif state == 'West Virginia':
+        print('Quading and ur like a very good state')
+    elif state == 'Virginia':
+        print('Another w state')
+    elif state == 'Wisconisn':
+        print('hmm another unknown')
+    elif state == 'Wyoming':
+        print('hmm another unknown')
+    else:
+        print('u spell wrong bud')
 
-if state == 'North Carolina':
-    print('look outside bro')
-elif state == 'Alabama':
-    print('ur fucking ur sister bro')
-elif state == 'Alaska':
-    print('snowing')
-elif state == 'Arizona':
-    print('ur making eggs in ur car w out a oven!!')
-elif state == 'California':
-    print('ur a fucking softie?!?!????!!????? idk i kinda forgor')
-elif state == 'Colorado':
-    print('ur skiing rn bro')
-elif state == 'Connecticut':
-    print('ur either rich or in the hood :skull:')
-elif state == 'Delaware':
-    print('unknown fr fr')
-elif state == 'Florida':
-    print('u swimming with sharks?? idk bro')
-elif state == 'Georgia':
-    print('ur growing mangos bruh STOP')
-elif state == 'Hawaii':
-    print('stop surfing')
-elif state == 'Idaho':
-    print('ur the hoe bruh')
-elif state == 'Illinois':
-    print('unknown fr :skull:')
-elif state == 'Indiana':
-    print('ur that one guy from that movie broooo')
-elif state == 'Iowa':
-    print('another unknown :frown:')
-elif state == 'Kansas':
-    print('kanasa city?!?!')
-elif state == 'Kansas':
-    print('Kansas city?!?!?')
-elif state == 'Kentucky':
-    print('Kentucky ballistics??')
-elif state == 'Louisiana':
-    print('river')
-elif state == 'Maine':
-    print('r u like fishing')
-elif state == 'Maryland':
-    print('marry me get it haha')
-elif state == 'Massachutetts':
-    print('ur the best')
-elif state == 'Michigan':
-    print('MIchiaginas')
-elif state == 'Mississippi':
-    print('another river')
-elif state == 'Missori':
-    print('unknown')
-elif state == 'Montana':
-    print('hmm another unknown')
-elif state == 'Nebraska':
-    print('hmm another unknown!!')
-elif state == 'Nevada':
-    print('gamble')
-elif state == 'New hampshire':
-    print('i love u bro pls fuck me till i leak all over the place :love:')
-elif state == 'New Jersy':
-    print('idk i forgor what u have')
-elif state == 'New Mexico':
-    print('Breaking bad')
-elif state == 'North Dakota':
-    print('hmm another unknown')
-elif state == 'Ohio':
-    print('Sigma skibidi toilet omg lvl 5 gyatt kai centait etc!!')
-elif state == 'Oklahoma':
-    print('Only in oklahoma')
-elif state == 'Oregon':
-    print('nice lakes and mountineasasasa')
-elif state == 'Pennsylvania':
-    print('Good mexican food for some reason')
-elif state == 'Rhode island':
-    print('that one beach ball game')
-elif state == 'South Carolina':
-    print('ass beach :skull: north myrtle better lmao')
-elif state == 'South Dakota':
-    print('hmm another unknown')
-elif state == 'Tennessee':
-    print('fgtv')
-elif state == 'Texas':
-    print('guns i love u')
-elif state == 'Utah':
-    print('hmm another unknown')
-elif state == 'Vermont':
-    print('haha ur nice')
-elif state == 'Washington':
-    print('idk candadaadadadada')
-elif state == 'West Virginia':
-    print('Quading and ur like a very good state')
-elif state == 'Virginia':
-    print('Another w state')
-elif state == 'Wisconisn':
-    print('hmm another unknown')
-elif state == 'Wyoming':
-    print('hmm another unknown')
-else:
-    print('u spell wrong bud')
-
-sleep(9000)
+    sleep(.75)
